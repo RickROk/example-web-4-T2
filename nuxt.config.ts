257afd1134eaf95fbe49ac92f32b22b9862f66f4
kzagul <zagulkirill@gmail.com>
2023-11-25 23:27:14 +0500

    added storybook

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,11 +2,16 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
   css: ["~/assets/css/main.css"],
-  modules: ["@pinia/nuxt", "nuxt-vitest"],
+  modules: ["@pinia/nuxt", "nuxt-vitest", "@storybook-vue/nuxt-storybook"],
   postcss: {
     plugins: {
       tailwindcss: {},
       autoprefixer: {},
     },
+  },
+  storybook: {
+    // Options
+    url: "http://localhost:6006",
+    port: 6006,
   },
 });
