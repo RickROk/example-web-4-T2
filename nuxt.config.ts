d5867b8f25edcf268f0776199d2c29421b0df11e
kzagul <zagulkirill@gmail.com>
2023-11-27 23:00:31 +0500

    updated project configuration

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,16 +2,17 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
   css: ["~/assets/css/main.css"],
-  modules: ["@pinia/nuxt", "nuxt-vitest", "@storybook-vue/nuxt-storybook"],
+  modules: ["@pinia/nuxt", "nuxt-vitest"],
+  // "@storybook-vue/nuxt-storybook"
   postcss: {
     plugins: {
       tailwindcss: {},
       autoprefixer: {},
     },
   },
-  storybook: {
-    // Options
-    url: "http://localhost:6006",
-    port: 6006,
-  },
+  // storybook: {
+  //   // Options
+  //   url: "http://localhost:6006",
+  //   port: 6006,
+  // },
 });
