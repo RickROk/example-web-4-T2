3aa0cf7d5b27cf2fa5cbd7e706fefacf046c1930
Vladislav Baranov <briliant645@gmail.com>
2023-11-28 11:27:02 +0500

    add card of Vladislav Baranov

--- components/teamGrid.vue
+++ components/teamGrid.vue
@@ -83,7 +83,7 @@
                       <a href="#">Баранов Владислав</a>
                   </h3>
                   <span class="text-gray-500 dark:text-gray-400">Web Developer</span>
-                  <p class="mt-3 mb-4 font-light text-gray-500 dark:text-gray-400">Michael drives the technical strategy of the flowbite platform and brand.</p>
+                  <p class="mt-3 mb-4 font-light text-gray-500 dark:text-gray-400">Начинающий веб-разработчик с большими перспективами.</p>
                   <ul class="flex space-x-4 sm:mt-0">
                       <li>
                           <a href="#" class="text-gray-500 hover:text-gray-900 dark:hover:text-white">
