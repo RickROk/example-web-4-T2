f1d7c43050358c2d8534c3b0427212659defcdf4
Vladimir Simonov <sim-steed2010@yandex.ru>
2023-12-05 17:59:44 +0300

    fix desription

--- components/card/TeamMemberDetail.vue
+++ components/card/TeamMemberDetail.vue
@@ -50,7 +50,7 @@ const props = withDefaults(defineProps<Props>(), {});
             <dt
               class="mb-2 font-semibold leading-none text-gray-900 dark:text-white"
             >
-              Биография
+              Фантазии
             </dt>
             <dd
               class="mb-4 font-light text-gray-500 sm:mb-5 dark:text-gray-400"
